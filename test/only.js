f8bc2ddbc0210a8f4657ee3bdfc5222a63369199
Darren Poon <dyhpoon@gmail.com>
2018-03-19 15:19:47 +0800

    support null for whitelisting

--- test/only.js
+++ test/only.js
@@ -1,29 +1,31 @@
-
+var should = require('should')
 var only = require('..');
 
 var obj = {
   name: 'tobi',
   last: 'holowaychuk',
   email: 'tobi@learnboost.com',
-  _id: '12345'
+  _id: '12345',
+  phone: null
 };
 
 var expected = {
   name: 'tobi',
   last: 'holowaychuk',
-  email: 'tobi@learnboost.com'
+  email: 'tobi@learnboost.com',
+  phone: null
 };
 
 describe('only(obj, keys)', function(){
   describe('given an array', function(){
     it('should return only the whitelisted properties', function(){
-      only(obj, ['name', 'email', 'last']).should.eql(expected);
+      only(obj, ['name', 'email', 'last', 'phone']).should.eql(expected);
     })
   })
 
   describe('given an string', function(){
     it('should return only the whitelisted properties', function(){
-      only(obj, 'name email last').should.eql(expected);
+      only(obj, 'name email last phone').should.eql(expected);
     })
   })
 
